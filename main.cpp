3e335110d181957bfe9b160fe94d905416111f0b
Ing. Di Filippo Davide <davidedifilippo@gmail.com>
2017-08-17 18:36:47 +0200

    original version without QIcon ico in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QIcon appIcon(":/Ico/Icons/serial_port_icon.icns");                       // Get the icon for the right corner
-    w.setWindowIcon(appIcon);
     w.setWindowTitle("Serial Port Plotter");
     w.show();
 
