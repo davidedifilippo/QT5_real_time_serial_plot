ad6502a7251ca35d5aa01394d52cecc9b9dfc449
Ing. Di Filippo Davide <davidedifilippo@gmail.com>
2017-08-18 16:29:07 +0200

    Added action menu bar icons

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
+
+
+
     MainWindow w;
     w.setWindowTitle("Serial Port Plotter");
+    QIcon appIcon(":/Icons/exe_icon.ico");
+    w.setWindowIcon(appIcon);
     w.show();
 
     return a.exec();  //main event loop
